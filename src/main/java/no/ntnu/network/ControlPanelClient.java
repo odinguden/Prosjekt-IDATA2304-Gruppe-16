36c8c5e2cdd7a6fc7b9de1732cdb5f6be0f9c713
Odin Lyngsgård <odin24@outlook.com>
2023-11-20 11:16:19 +0100

    Update ControlPanelClient.java

--- src/main/java/no/ntnu/network/ControlPanelClient.java
+++ src/main/java/no/ntnu/network/ControlPanelClient.java
@@ -3,14 +3,20 @@ package no.ntnu.network;
 import java.io.IOException;
 
 import no.ntnu.listeners.common.ActuatorListener;
+import no.ntnu.listeners.common.CommunicationChannelListener;
 import no.ntnu.sigve.client.Client;
 
 /**
  *
  */
-public class ControlPanelClient extends Client{
+public class ControlPanelClient implements CommunicationChannelListener{
 
 	public ControlPanelClient(String address, int port) throws IOException {
-		super(address, port);
+	}
+
+	@Override
+	public void onCommunicationChannelClosed() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'onCommunicationChannelClosed'");
 	}
 }
